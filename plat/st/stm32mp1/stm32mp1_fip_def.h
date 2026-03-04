aa9e0c78b9070d03c55a9190dbc83b83f0ca4384
onedots <onedots0@outlook.com>
2026-03-04 22:30:29 +0800

    fix: remove obsolete nvmem_layout node and increase BL2 size for v2.10

--- plat/st/stm32mp1/stm32mp1_fip_def.h
+++ plat/st/stm32mp1/stm32mp1_fip_def.h
@@ -49,8 +49,8 @@
 #endif
 #endif /* STM32MP13 */
 #if STM32MP15
-#define STM32MP_BL2_RO_SIZE		U(0x00011000)	/* 68 KB */
-#define STM32MP_BL2_SIZE		U(0x00016000)	/* 88 KB for BL2 */
+#define STM32MP_BL2_RO_SIZE		U(0x00014000)	/* 80 KB */
+#define STM32MP_BL2_SIZE		U(0x0001B000)	/* 108 KB for BL2 */
 #endif /* STM32MP15 */
 #endif /* TRUSTED_BOARD_BOOT && !STM32MP_USE_EXTERNAL_HEAP */
 
